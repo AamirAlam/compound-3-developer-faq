ce264b04b80d3a267e1e7496ec55c2b0f5336ec7
Aamir Alam <aamiralam1991@gmail.com>
2024-07-31 13:48:11 +0530

    add eth-sepolia local testing

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,41 +1,49 @@
-require('@nomiclabs/hardhat-ethers');
+require("@nomiclabs/hardhat-ethers");
 
 // Choose the Comet instance to run tests against using the `connections` object
-const cometInstance = 'usdc-polygon';
+const cometInstance = "usdc-sepolia";
 
 // Optionally, you can hardcode provider URLs here
 const connections = {
-  'usdc-mainnet': {
-    providerUrl: process.env.MAINNET_PROVIDER_URL,
+  "usdc-mainnet": {
+    providerUrl: "https://eth.llamarpc.com",
     // blockNumber: 16192000, // 2022-12-15T18:51:47.000Z
     blockNumber: 17330000, // 2023-05-24T03:41:11.000Z
     chainId: 1,
   },
-  'usdc-goerli': {
+  "usdc-goerli": {
     providerUrl: process.env.GOERLI_PROVIDER_URL,
     blockNumber: 8141000, // 2022-12-15T19:00:48.000Z
     chainId: 5,
   },
-  'usdc-polygon': {
+  "usdc-polygon": {
     providerUrl: process.env.POLYGON_PROVIDER_URL,
     blockNumber: 48949630, // 2023-10-20T16:23:02.000Z
     chainId: 137,
-  }
+  },
+  "usdc-sepolia": {
+    providerUrl: "https://rpc-sepolia.rockx.com",
+    blockNumber: 6408386, // 2023-10-20T16:23:02.000Z
+    chainId: 137,
+  },
 };
 
 const { providerUrl, blockNumber, chainId } = connections[cometInstance];
 
 if (!providerUrl) {
-  console.error('Cannot connect to the blockchain.');
-  console.error('Add a provider URL in the hardhat.config.js file.');
+  console.error("Cannot connect to the blockchain.");
+  console.error("Add a provider URL in the hardhat.config.js file.");
   process.exit(1);
 }
 
 // Do not use this mnemonic outside of localhost tests!
-const mnemonic = 'romance zebra roof insect stem water kiwi park acquire domain gossip second';
+const mnemonic =
+  "romance zebra roof insect stem water kiwi park acquire domain gossip second";
 
 if (!providerUrl) {
-  console.error('Missing JSON RPC provider URL as environment variable. See hardhat.config.js.');
+  console.error(
+    "Missing JSON RPC provider URL as environment variable. See hardhat.config.js."
+  );
   process.exit(1);
 }
 
@@ -44,13 +52,13 @@ module.exports = {
   testProviderUrl: providerUrl,
   testBlockNumber: blockNumber,
   solidity: {
-    version: '0.8.11',
+    version: "0.8.11",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 1000
-      }
-    }
+        runs: 1000,
+      },
+    },
   },
   networks: {
     hardhat: {
@@ -63,11 +71,11 @@ module.exports = {
       initialBaseFeePerGas: 0,
       loggingEnabled: false,
       accounts: {
-        mnemonic
+        mnemonic,
       },
     },
   },
   mocha: {
-    timeout: 60000
-  }
+    timeout: 60000,
+  },
 };
